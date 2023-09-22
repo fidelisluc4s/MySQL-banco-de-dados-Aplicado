8402d6d62e4c9ebbd8f4c8db2c6cbb81d3a95a91
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-09-22 01:02:28 -0300

    Junção na cláusula where

---
+++ livraria/Exercicio de  livraria.sql
@@ -0,0 +1,14 @@
+/*QUESTÃO 01: Recupere os tipos de serviços e valor, para os serviços que custam mais de R$50,00.(6 linhas retornadas)*/
+use livraria;
+
+select 
+from 
+where;
+
+/*QUESTÃO 02: Recupere o nome do animal juntamente com sua raça.(1008 linhas retornadas)*/
+
+/*QUESTÃO 03: Recupere todas as informações dos animais que possuem alto nível de cuidados.(375 linhas retornadas)*/
+
+/*QUESTÃO 04: Recupere o nome do cliente e o tipo de serviço que ele solicitou. Traga apenas os serviços que ainda não foram pagos.(469 linhas retornadas)*/
+
+/*QUESTÃO 05: Recupere as informações das clientes, o nome e a raça dos animais que elas possuem e o tipo de serviço que foi realizado. Elimine os valores repetidos. (948 linhas retornadas)*/
