32cfa917625836b82b08ddab919c04c6016b7b05
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-12-20 09:54:46 -0300

    Update exercicio reserva.sql

--- reserva/exercicio reserva.sql
+++ reserva/exercicio reserva.sql
@@ -72,5 +72,66 @@ where nomecidade = 'rio de janeiro';
 /*⦁	Recupere o prefixo, o fabricante e a quantidade de voos 
 feitos por cada aeronave.*/
 
-select *
-from aeronave 
+select a.prefixo, a.fabricante, count(*) as qnt_voo
+from aeronave a
+inner join voo v on a.codigoaeronave = v.codigoaeronave
+group by v.codigoaeronave
+having count(*) > 2;
+
+/*⦁	Recupere o valor total das reservas feitas para o 
+voo de número CON-26.*/
+
+select v.numerovoo, sum(preco)
+from voo v
+inner join reserva r on v.codigovoo = r.codigovoo
+where numerovoo = "con-26"
+group by numerovoo;
+
+/*Recupere todos os dados dos clientes que efetuaram 
+ou não alguma reserva, assim como o número da reserva.*/
+
+select c.*, codigoreserva
+from cliente c
+left join reserva r on c.codigocliente = r.codigocliente;
+
+/*Crie e exiba uma visão que recupere a média dos preços e a 
+quantidade das reservas de assentos de classe econômica.*/
+create view qnt_media_preco_assentos_economicos as
+select a.*,avg(preco), count(*)
+from reserva r
+inner join assento a on r.numeroassento = a.numeroassento
+where classe = "economica"
+group by a.numeroassento;
+
+
+/*	Crie um usuário no banco de dados e dê permissão de visualização
+ apenas da visão criada no item anterior.*/ 
+
+create user 'Piloto' identified by '123456';
+grant select on reserva.qnt_media_preco_assentos_economicos to 'Piloto'@'%';
+
+/*Crie uma trigger que reduza na tabela voo a quantidade de assentos 
+livres quando uma inserção é feita na tabela reserva.*/
+
+/*inserindo para teste*/
+INSERT INTO voo (codigovoo, numerovoo, codigoaeronave, cidadeorigem,
+ cidadedestino, horariosaida, horariochegada, assentosLivres) 
+VALUES ('1010', 'CON-22', '1111', '2345', '1234', '04:11:00', '05:11:00', 
+'100');
+
+update voo 
+set assentosLivres = assentosLivres -1
+where codigovoo= '1000' and assentosLivres=-90;
+
+delete from voo where codigovoo= '1000' and assentosLivres=90 limit 1;
+
+
+DELIMITER $$
+CREATE TRIGGER atualizaAssento AFTER INSERT ON reserva
+FOR EACH ROW
+BEGIN
+	UPDATE voo SET assentosLivres = new-assentosLivres
+    WHERE codigovoo = new.codigovoo;
+END $$
+
+drop trigger atualizaAssento;
